aca9e50173d14922061f7c1bc67d3a5eaff835f0
Suzuki Masaya <ikachang-kawaii@nifty.com>
2025-09-23 09:21:26 +0900

    Add customers page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,24 @@
 import { Metadata } from "next";
+import Table from "@/app/ui/customers/table";
+import { fetchFilteredCustomers } from "@/app/lib/data";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "Customers",
 };
 
-export default function Page() {
-  return <p>Customers Page</p>;
+export default async function Page(props: {
+  searchParams?: Promise<{ query?: string }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || "";
+  const customers = await fetchFilteredCustomers(query);
+
+  return (
+    <div className="w-full">
+      <Suspense fallback={<div>Loading...</div>}>
+        <Table customers={customers} />
+      </Suspense>
+    </div>
+  );
 }
